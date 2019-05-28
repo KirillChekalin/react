8757da82ba8225730860069b64c24743f954f86a
Kirill <kir.chekalin@gmail.com>
2019-05-28 10:26:16 +0300

    added decorator to article

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,45 +1,37 @@
 import React, {Component} from 'react'
 import CommentList from './comments/CommentList';
+import toogleOpen from './decorator/toogleOpen'
 
 class Article extends Component{
-  state = {
-      isOpen: true
-    };
-
 
   render() {
-    const {article} = this.props;
-    // if (article.comments) console.log(article.comments[0]);
-    // const body = this.state.isOpen && <section className='card-text'>{article.text}</section>;
-
+    const {article, isOpen, toggleOpen} = this.props;
     return (
+
       <div className='card mx-auto' >
         <div className='card-header'>
           <h2>
             {article.title}
-            <button onClick = {this.handleClick} className='btn btn-primary btn-lg float-right'>
-              {this.state.isOpen ? 'close' : 'open'}
+            <button onClick = {toggleOpen} className='btn btn-primary btn-lg float-right'>
+              {isOpen ? 'close' : 'open'}
             </button>
           </h2>
         </div>
         <div className='card-body'>
           {this.getBody()}
         </div>
-        <CommentList comments = {this.state.isOpen ? article.comments : null}/>
+        <CommentList comments = {isOpen ? article.comments : null}/>
       </div>
+
     )
   }
   getBody() {
-    if(!this.state.isOpen) return null;
-    const {article} = this.props;
+    const {article, isOpen} = this.props;
+    if(!isOpen) return null;
     return <section>{article.text}</section>;
   }
-  handleClick = () => {
-    this.setState({
-      isOpen: !this.state.isOpen
-    });
-  }
+
 }
 
 
-export default Article
+export default toogleOpen(Article)
