2a88147eb70869de265852c3713dc984f61f2536
Kirill <kir.chekalin@gmail.com>
2019-06-13 13:07:27 +0300

    add logger(middleware)

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,8 +1,12 @@
 import {normalizedComments as defaultComments} from '../components/fixtures.js'
 import {DELETE_ARTICLE, FILTER_ARTICLE} from '../constans'
 
+const commentsMap = defaultComments.reduce((acc, comment) => {
+  acc[comment.id] = comment;
+  return acc;
+}, {});
 
-export default (commentsState = defaultComments, action) => {
+export default (commentsState = commentsMap, action) => {
   const {type, payload} = action;
   switch (type) {
   }
--- src/reducer/immutableArticles.js
+++
@@ -1,5 +0,0 @@
-import {articles as defaultArticles} from '../components/fixtures.js'
-
-export default (immutableArticles = defaultArticles, action) => {
-  return immutableArticles;
-};
